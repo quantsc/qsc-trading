02957a7cf4c678f16df3e9346f57216128ff6220
Daniel Bohen <bohen@usc.edu>
2023-02-14 17:11:05 -0800

    feat: added basic risk files

---
+++ qsc-trading/order-manager/include/risk.hpp
@@ -0,0 +1,13 @@
+//==-- Risk.hpp - Calculating Portfolio Risk Exposure -==//
+//
+// Part of the Q
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Defines CGFunctionInfo and associated types used in representing the
+// LLVM source types and ABI-coerced types for function arguments and
+// return values.
+//
+//===----------------------------------------------------------------------===//
